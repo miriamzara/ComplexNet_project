b5046d3b2be6de568cf2f1e7cea91ffb4fb03c0f
Miriam Zara <miriamzara@MacBook-Pro-di-Miriam.local>
2024-07-12 19:32:03 +0200

    dinner break

--- project_01/ising.py
+++ project_01/ising.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.signal import find_peaks
 
 def compute_energy(adjacency_matrix, state, J = 1.0, h = 0.0):
     energy = -h * np.sum(state)
@@ -16,8 +17,8 @@ def simulate_ising_fixed_temp(adjacency_matrix, T, num_steps, n_snapshots = 0, J
     data = np.zeros((num_steps, 3))
     state = initial_state.copy()
     for step in range(num_steps):
-        if step % 50 == 0:
-            print(f"step number {step}")
+        if step % 1000 == 0:
+            print(f"temperature {T}, step number {step}")
         """
         metropolis update 
         consists in N node updates (N == network size)
@@ -51,7 +52,8 @@ def simulate_ising(adjacency_matrix, T_i, T_f, t_points, equilibration_steps, sw
     temperatures = np.linspace(T_i, T_f, t_points)
     data = np.zeros((t_points, 9))
     for t, T in enumerate(temperatures):
-        print(f"temperature {T}, point {t}/{t_points}")
+        if t % 10 == 0:
+            print(f"temperature {T}, point {t + 1}/{t_points}")
         energies = np.zeros(sweep_steps)
         magnetizations = np.zeros(sweep_steps)
         initial_state = np.random.choice([-1, 1], size = num_nodes)
@@ -81,3 +83,35 @@ def simulate_ising(adjacency_matrix, T_i, T_f, t_points, equilibration_steps, sw
         data[t] = [T, energy, std_energy, np.abs(magnetization), std_magnetization,  specific_heat, std_specific_heat, susceptibility, std_susceptibility]
     df = pd.DataFrame(data, columns=['temperature', 'energy', 'std_energy' ,'abs_magnetization', 'std_magnetization', 'heat', 'std_heat', 'susceptibility', 'std_susceptibility'])
     return df
+
+
+def estimate_temperature(df):
+
+    # METHOD 1: maximum derivative of magnetization and energy
+    dM_dT = np.gradient(df['abs_magnetization'], df['temperature'])
+    Tc_magnetization = df['temperature'].iloc[np.argmax(np.abs(dM_dT))]
+
+    dE_dT = np.gradient(df['energy'], df['temperature'])
+    Tc_energy = df['temperature'].iloc[np.argmax(np.abs(dE_dT))]
+
+    # METHOD 2: Fit of peaks in magnetic susceptibility and heat capacity
+    local_peaks_indices, _ = find_peaks(df['susceptibility'])
+    local_peak_temperatures = df['temperature'].iloc[local_peaks_indices]
+    local_peak_values = df['susceptibility'].iloc[local_peaks_indices]
+    local_peaks_dict = {index: value for index, value in zip(local_peaks_indices, local_peak_values)}
+    max_value = max(local_peaks_dict.values())
+    max_index = [index for index, value in local_peaks_dict.items() if value == max_value][0]
+    Tc_susceptibility = df['temperature'].iloc[max_index]
+
+    local_peaks_indices, _ = find_peaks(df['heat'])
+    local_peak_temperatures = df['temperature'].iloc[local_peaks_indices]
+    local_peak_values = df['heat'].iloc[local_peaks_indices]
+    local_peaks_dict = {index: value for index, value in zip(local_peaks_indices, local_peak_values)}
+    max_value = max(local_peaks_dict.values())
+    max_index = [index for index, value in local_peaks_dict.items() if value == max_value][0]
+    Tc_heat = df['temperature'].iloc[max_index]
+
+    Tc_estimates = np.array([Tc_magnetization, Tc_energy, Tc_susceptibility, Tc_heat])
+    Tc_average = np.mean(Tc_estimates)
+    Tc_std = np.std(Tc_estimates)
+    return Tc_average, Tc_std
